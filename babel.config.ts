cedc9a34461c39e85a5d9dc3bd46341f1f1609e5
Sidharth PT <sidharthanand73@gmail.com>
2024-11-28 07:58:27 +0530

    feat: initial setup

---
+++ babel.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+module.exports = function (api: { cache: (arg0: boolean) => void; }) {
+    api.cache(true);
+    return {
+      presets: [
+        ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+        "nativewind/babel",
+      ],
+    };
+  };
